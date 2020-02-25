9e4bbb09b3ee5d4ae703bf59254bc2e66035513d
Rune Alexander <alexander.laursen@gmail.com>
2020-02-25 19:58:25 +0100

    update

--- Juice.sh
+++ Juice.sh
@@ -1,224 +1,28 @@
 #!/bin/bash
 resize -s 56 100
-a=0
-cd Juice/
-clear
-echo -e '
 
-Checking for updates on Juice:'
-git pull
 cd ~/dat219g20v/assignments
 echo -e '
-
 Checking for updates on Bamboo:'
 git pull
-while (($a <= 1))
-do
-echo -n '
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhsoo/-/oss++oooooooooooooooooooooooooooooo
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhsoo:-:/++//+ooooooooooooooooooooooooooooo
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhyo/::----------://++++ooooooooooooooooooooo
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs+:-:::::::::::::::----:://ooooooooooooooooooo
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhy/-:::::::::::::::::::::::::---/+ooooooooooooooo
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs/--:::::::::::::::::::::::::::::--/+ooooooooooooo
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhho-:::::::--::////:::--:::::::::::::---/oooooooooooo
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs--::::/+oyhdmmmmmmddhys+/:::::::::::::-:+oooooooooo
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhho-:-+ydmmmmmmmmmmmmmmmmmmdds+:-:::::::::--/ooooooooo
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhy-ohmmmmmmmmmmmmmmmmmmmmmmmmmds/::::::::::-+oooooooo
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhyoymmmmmmmmmmmmmmdddddmmmmmmmmmmms:-:::::::-:oooooooo
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhyyhhdddhhmmmmmmmdhhdmmmmmdhdmmmmmmmmmh+-::::::--+ooooo+:
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhyymMMMMMMNmydmmmmhhNMMMMMMMMNmydmmmmmmmmd+-::::::./oooo+:-
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhyyMMMMMMMMMMNydmmhdMMMMMMMMMMMMNydmmmmmmmmd/-:::::-:oo+/---
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhsmMmdMMMMMMMMdhmmyNMMMNmMMMMMMMMmymmmmmmmmmh::::::-.o+:----
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhomMmdMMMMMMMMdhmmyNMMMmhNMMMMMMMmymmmmmmmmmm+-:::::--:---:o
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhyyNMMMMMMMMMmydmmdhNMMMMMMMMMMMNhdmmmmmmmmmmy-:::::-.---/ss
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhyodNNMMMNmhhdmmmmdhdNMMMMMMMNmhdmmmmmmmmmmmh-::::-.---+sss
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhyoddddddddhyhmmmmmmdhdddmmddhdmmmmmmmmmmmddh-:::-.--:ossss
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhodmmmmmmmoddhhmmmmmmmmmmmmmmmmmmmmmmmmmmddy/:::.--:ossss+
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh+hmmmmmmmhhhdmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmh:-..-/sssso+/
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs+mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmh:.-:+sssso///
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhyss+smmmmdyyosy+yyshsydmmmmmmmmmmmmmmmmmmddhy/--/ossso+///+
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhyssso+sdmm/-o-/+:o/+os+/dmmmmmmmmmmmmmmmmd+-:--:+sssss+//+++
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhsssssss+ohmdhdhhhhhyhyhhhmmmmmmmmmmmmmmmmh+----:ososso///++++
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhysssoosssso+ohmmmmmmmmmmmmmmmmmmmmmmmmmmmho:----/sssss+//+/++++
-hhhhhhhhhhhhhhhhhsyhhhhhhhhhhhhhhhhhsssosssssssssoooshmmmmmmmmmmmmmmmmmmmmdhs/------+sssss+//+++++++
-hhhhhhhhhhhhhhhh+yohhhhhhhhhhhhhhhysssssssssssssssso:-/shddddddddddddhhhs/:----:/:-:+osss+//////++++
-hhhhhhhhhhhhhhhy+dsyhhhhhhhhhhhhhysssssssssssssssoo:../dmmddddddddddmmms-----:oo/////:+o//+++//+++++
-hhhhhsssyyyyyyyyymhohhhhhhhhhhhhyossssssssoossosso:.--hNmmmmmmmNNmmmNNy-----+dmms//++/:-//++++++++++
-hhhhhssyssoyyyhhhmmyoyhhhhhhhhysoossossssssossooo+.-.-dNmmNNmmNNNmmNNm:----/mmmmmo/////-:/++++++++++
-hhhhhhhhhh+syso+ymmmdysyhhhhhyssssossssssssoooooho.-.omNmmNNmmmmmmmmms.---.yNmmmNm+//:/:+-/+++++++++
-hhhhhhhhhhooyso/hdmmmmdysyhhyssssssssssssssooohmd:--.dNNNNNNmmmmmNmmm/-----hNmmNNNh//:///.://+++++++
-hhhhhhhhhhysysooysydmmmmdyssossssssssssssso+ymNmd.--/NNNNNNNmmmmmNNNd:-----hNmmmNmm+/-///:-/////++++
-hhhhhhhhhhhhhhhhhhysydmmmmmyo+osssssssssooodmmmmh.-.yNNmNNNmmmmmmNNNm/-----yNmmmNmNy:://:/-/+///++++
-hhhhhhhhhhhhhhhhhhhhyoydmmmmmhooossssso++hmmmNNNd.--hNmmmmmmmmmmmNmmmo.----/mmmmNmmm:://:/:-+//+++++
-hhhhhhhhhhhhhhhhhhhhhhyosdmmmmmhs+osso++hhhdmNmNd:-:dNmmmmmmmmmmmNmmNd------mNNNNmmm:///:/:-:/++++++
-hhhhhhhhhhhhhhhhhhhhhhyso+sdmmmmmds++ohmmddhsdNmy-./mNmmmmmmmmmmmNNmmm+.----mNNmmmNN+://://--:/+++//
-hhhhhhhhhhhhhhhhhhhhhysssso+sdmmmmmdshmmmmdyyyhoo/./mNmmmmmmmmmmmNNNmNm:----mNNNmNNNo-/+///::-:::://
-hhhhhhhhhhhhhhhhhhhhysosoosso+sdmmmmmmmmdy++s++soo./mNmmmmmmmmmmmNNNmmmh:---mNNmmmmmo-:+///::-------
-hhhhhhhhhhhhhhhhhhhysssssssssoo+odmmmmmy++osssssoo-/NNmmmmmmmmmmmmNNNmmNd:--mNNmmmNms-:+////:-------
-hhhhhhhhhhhhhhhhhyssssssssssossso+oyhyo+osssssssss++NmmmmmmmmmmNNmmmNNmmNm+.oyhdhhho/-//////:-------
-sssssssssssssssss+++++++++++++++++++/++++++++++++o/oNmmmmmmmmmmNNmmmNNmmmmmy--hmmmms/://////::------
-ooooossssssssssoooooosoooossoooooooooooooooooooooo+sNNmmmmmmmmmmmmmmmmmmmmmNd+hmmmms::://////:------
-oooooooooooooooooooooooooo++++++++++++++++++++++++/sNNmmmmmmmmmmmmmmmmmmmmmmNhdmmmmy..:////++:------
-....................``````````````````````````````.yNmmmmmmmmmmmmmmmmmmmmmmmNhhmmmmy.-.:/+//+-------
-...................................................yNmmmmmmmmmmmmmmmmmmmmmmmNhhmmmmy.--.:+:/+-------
----------------------------------------------------hNmmmmmmmmmmmmmmmmmmmmmmmNhhmmmmy----::/++-.----+
----------------------------------------------------hNmmmmmmmmmmmmmmmmmmmmmmmNhhmmmmy/////+/+/-`...-h
----------------------------------------------------dNmmmmmmmmmmmmmmmmmmmmmmmNhhmmmms//++//++/.`.``-d
 
-VERSION-24.02.20.12.15
+cd ~/Juice/
+head=$(git rev-parse HEAD)
+source VERSION.txt
+
+echo -e '
+Checking for updates on Juice:'
+if [ $VERSION != $head ]
+then
+git pull
+head=$(git rev-parse HEAD)
+rm VERSION.txt
+
+echo VERSION=$(git rev-parse HEAD) > VERSION.txt
+fi
+echo -e 'Already up to date.
 '
-read -p '1. GIT PUSH
-2. Test
-3. Lag nytt
-4. Slett
-5. Last ned Teacher-course
-6. EXIT
-Valg: ' FIRST
 
-if  [ $FIRST == 1 ] 
-then
-cd ~/solutions/
-git add .
-git commit -m "Added and updated assignments"
-git push origin master
-fi
-
-if [ $FIRST == 3 ] 
-then
-clear
-read -p 'Skriv navn på prosjekt, eller stopp for å avbryte ' PR 
-
-if [ $PR != 'stopp' ]
-then 
-clear
-read -p 'type oppgave?
-1. MVC - None
-2. WebApp
-3. MVC - Individual
-Valg: ' VALG
-
-if [ $VALG == 1 ]
-then
-cd ~/dat219g20v/assignments/solutions
-dotnet new mvc --auth None -o $PR
-fi
-
-if [ $VALG == 2 ]
-then
-cd ~/dat219g20v/assignments/solutions
-dotnet new webapp --auth None -o $PR
-fi
-
-if [ $VALG == 3 ]
-then
-cd ~/dat219g20v/assignments/solutions
-dotnet new mvc --auth Individual -o $PR
-fi
-
-cd $PR
-
-echo -n 'Legger til: dotnet package Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation v3.1.1.........'
-dotnet add package Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation --version 3.1.1
-
-echo -n 'Legger til: package Microsoft.EntityFrameworkCore.Sqlite...........'
-dotnet add package Microsoft.EntityFrameworkCore.Sqlite
-
-if [ $VALG != 3 ]
-then
-sed -i '' -e "26s/^//p; 26s/^.*/            services.AddRazorPages().AddRazorRuntimeCompilation();/" startup.cs
-fi
-if [ $VALG == 3 ]
-then
-sed -i '' -e "36s/^//p; 36s/^.*/            services.AddRazorPages().AddRazorRuntimeCompilation();/" startup.cs
-dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design
-dotnet add package Microsoft.EntityFrameworkCore.Design
-dotnet add package Microsoft.EntityFrameworkCore.SqlServer
-fi
-
-fi
-fi
-
-if [ $FIRST == 4 ] 
-then
-cd ~/dat219g20v/assignments/solutions
-clear
-echo -e '--------------...'
-ls
-echo -e '--------------...'
-read -p 'prosjekt som skal slettes:' DELETE
-rm -r $DELETE
-fi
-
-if [ $FIRST == 5 ] 
-then
-clear
-read -p 'Dette vil overskrive teacher-course mappe, trykk enter for å fortsette, eller F for å avbryte' FF
-
-if [[ $FF != 'F' ]]
-then
-cd ~/dat219g20v/assignments/#teacher-course
-#git pull
-rm -r -f teacher-course
-git clone ssh://git@tools.uia.no:7999/dat219g20v/teacher-course.git
-fi
-
-fi
-
-if [ $FIRST == 6 ] 
-then
-clear
-echo -n 'Cleaning up docker images!....
-'
-docker system prune -f
-sleep 2
-echo -n 'closing program'
-exit
-fi
-
-if [ $FIRST == 7 ]
-then
-cd 
-cd dat219g20v/
-git add Juice.sh
-git commit -m "update"
-git push
-fi
-
-if  [ $FIRST == 2 ]
-then
-clear
-read -p 'Oppgave som skal testes: assignment_' ASS
-while (($a <= 1))
-do
-
-echo -n '1. Test hele koden
-2. Velg hvilken del
-3. Tilbake
-'
-read -p '' CH
-
-if [ $CH == 1 ]
-then
-cd ~/dat219g20v/assignments/
-./test.sh assignment_$ASS
-fi
-
-if [ $CH == 2 ]
-then
-read -p 'Skriv inn del-test test_' DT
-cd ~/dat219g20v/assignments/
-./test.sh assignment_$ASS -k test_$DT
-fi
-
-if [ $CH == 3 ]
-then
-break
-fi
-
-done
-fi
-
-done
+./Program.sh
 
 #end
